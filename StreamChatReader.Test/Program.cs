4735a0ee801f18d7cc11797db3246491b103440d
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2023-03-23 23:21:48 -0400

    main

---
+++ StreamChatReader.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using StreamingServices;
+using StreamingServices.Chat;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Text;
+
+double money = 0;
+
+ChatReader ChatReader = new();
+ChatReader.ChatEvent += ChatReader_ChatEvent;
+ChatReader.ChatLoaded += ChatReader_ChatLoaded;
+
+void ChatReader_ChatLoaded(ChatLoadedArgs e)
+{
+    Debug.WriteLine("Loaded/Linked");
+}
+
+void ChatReader_ChatEvent(ChatEventArgs e)
+{
+    Console.WriteLine(e.Message);
+}
+
+ChatReader.AddYoutubeStream("bDeysRZ3PJI");
+
+while(true)
+    Console.ReadKey();
